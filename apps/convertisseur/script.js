041a0d276c62cc1ddfab2424a261369926ccfc01
Éloi Draï <eloidrai@gmail.com>
2019-01-14 20:52:00 +0100

    Ajout de la version HTML5 de dtv

---
+++ apps/convertisseur/script.js
@@ -0,0 +1,42 @@
+var d = document.getElementById('magique');
+var v = document.getElementById('valeur');
+var r = document.getElementById('resultat');
+var l1 = document.getElementById('legende1');
+var n1 = document.getElementById('valeur1');
+var l2 = document.getElementById('legende2');
+var n2 = document.getElementById('valeur2');
+
+function init(){
+    d.style.visibility = 'visible';
+    switch(v.value){
+        case 'd':
+            l1.innerHTML = "Temps (s):";
+            l2.innerHTML = "Vitesse (m/s):";
+            break;
+        case 't':
+            l1.innerHTML = "Distance (m):";
+            l2.innerHTML = "Vitesse (m/s):";
+            break;
+        case 'v':
+            l1.innerHTML = "Distance (m):";
+            l2.innerHTML = "Temps (s):";
+            break;
+        }
+    n1.value = 0;
+    n2.value = 0;
+    r.innerHTML= "";
+}
+
+function calc(){
+    switch(v.value){
+        case 'd':
+            r.innerHTML = String(Number(n1.value)*Number(n2.value))+" mètres";
+            break;
+        case 't':
+            r.innerHTML = String(Number(n1.value)/Number(n2.value))+" secondes";
+            break;
+        case 'v':
+            r.innerHTML = String(Number(n1.value)/Number(n2.value))+" mètres par seconde";
+            break;
+    }
+}
