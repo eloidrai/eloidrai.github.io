4d2d95670682dd471b7f1ff6834c25c1e38f8649
Éloi Draï <eloidrai@gmail.com>
2019-01-31 18:46:52 +0100

    Simplification du JavaScript

--- apps/multiplications/script.js
+++ apps/multiplications/script.js
@@ -1,15 +1,11 @@
-var table = document.getElementById("table");
-var message = document.getElementById("message");
-var question = document.getElementById("question");
-var menu = document.getElementById("menu");
-var reponse = document.getElementById("reponse");
-var tableau = document.getElementById("tableau");
-var etapes = 0;
-var score = 0;
-var facteur;
+var table = document.getElementById("table"), message = document.getElementById("message"), menu = document.getElementById("menu"), tableau = document.getElementById("tableau");
+var facteur, question, reponse;
+var etapes = 0, score = 0;
+
 table.focus();
 
-function jeu(){
+
+function debut(){
     if (table.value == ""){
         alert("Veuillez choisir un nombre");
     } else {
@@ -18,12 +14,17 @@ function jeu(){
     }
     demander();
 }
+
 function demander(){
+    message.innerHTML = "<span id=\"question\"></span><br/><input type=\'number\' id=\"reponse\" onkeydown=\"if (event.keyCode == 13){verifier();}\" />" ;
+    question = document.getElementById("question");
+    reponse = document.getElementById("reponse");
     reponse.value = "";
     reponse.focus();
     facteur = Math.floor(Math.random()*10);
     question.innerHTML = table.value+"*"+facteur;
 }
+
 function verifier(){
     if (etapes<5){
         if (reponse.value==facteur*table.value){
@@ -33,8 +34,8 @@ function verifier(){
             alert("Non, c'était "+facteur*table.value+".");
         }
         demander();
+        etapes++;
     } else {
         message.innerHTML = "<mark>Score : "+score+"/5 </mark><br/><a href=\"index.html\" id=\"rejouer\">Rejouer</a>";
     }
-    etapes++;
 }
